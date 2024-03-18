2f731956369d750abb01029d7a8c59e862f1e523
Chris Vandborg <cvand15@student.sdu.dk>
2024-03-18 21:48:00 +0100

    Update PQHeap.py

--- PQHeap.py
+++ PQHeap.py
@@ -15,10 +15,8 @@ from typing import List
 def parent(i: int) -> int:
     """Return the index of the parent node of i from a heap like data structure
 
-    Parameters
-    ---------
-    i: int, default None
-        An index of a node in heap like priority queue data structure.
+    i
+        index of a node in heap like priority queue data structure, default is None.
     :return: int
     """
     return (i - 1) // 2
@@ -27,10 +25,8 @@ def parent(i: int) -> int:
 def left(i: int) -> int:
     """Return the index of the left child node of i from a heap like data structure
 
-    Parameters
-    ---------
-    i: int, default None
-        An index of a node in heap like priority queue data structure.
+    i
+        index of a node in heap like priority queue data structure, default is None.
     :return: int
     """
     return (2 * i) + 1
@@ -39,10 +35,8 @@ def left(i: int) -> int:
 def right(i: int) -> int:
     """Return the index of the right child node of i from a heap like data structure
 
-    Parameters
-    ---------
-    i: int, default None
-        An index of a node in heap like priority queue data structure.
+    i
+        index of a node in heap like priority queue data structure, default is None.
     :return: int
     """
     return (2 * i) + 2
@@ -52,11 +46,9 @@ def extract_min(priority_q: List[int]) -> int:
     """
     Return the key of the element with the least priority from a heap like priority queue data structure
 
-    Parameters
-    ---------
-    priority_q, default = None
-        A non-empty list of elements in a heap like data structure
-    :return: None
+    priority_q
+        A non-empty list of elements in a heap like data structure, Default is None
+    :return: int
     """
     # TODO
     pass
@@ -70,12 +62,10 @@ def insert(priority_q: List[int], num: int) -> None:
     """
     Insert key into priority_q.
 
-    Parameters
-    ---------
-    priority_q, default = None
-        A non-empty list of elements in a heap like data structure
-    num, default = None
-        An integer value that will be inserted into priority_q
+    priority_q
+        A non-empty list of elements in a heap like data structure, Default is None
+    num
+        An integer value that will be inserted into priority_q, Default is None
     :return: None:
     """
     priority_q.append(num)
@@ -91,8 +81,6 @@ def create_emptyPQ():
     """
     Return an empty heap like priority queue data structure
 
-    parameters
-    ----------
     :return: Empty priority queue
     """
 
